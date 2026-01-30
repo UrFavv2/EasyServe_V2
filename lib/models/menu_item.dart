f63b27e62f65c2342d62e363ed697db6baebb0df
urfavv2 <k592984442@gmail.com>
2026-01-29 21:58:09 -0800

    add menu_item, update main.dart

---
+++ lib/models/menu_item.dart
@@ -0,0 +1,12 @@
+// lib/data/constants.dart
+
+import 'package:flutter/material.dart';
+
+// 🌟 ဒီ List ကြီးကို တစ်နေရာတည်းမှာပဲ ထားပါ
+// ဒါဆိုရင် Screen တွေ အကုန်လုံးက ဒီ List တစ်ခုတည်းကိုပဲ လှမ်းကြည့်ကြမှာပါ
+List<Map<String, dynamic>> sharedMenuList = [
+  {"name": "Special Fried Rice", "isAvailable": true},
+  {"name": "Spicy Ramen", "isAvailable": true},
+  {"name": "Chicken Curry", "isAvailable": true},
+  {"name": "Coca Cola", "isAvailable": true},
+];
